7642913cd0d5f307e316c03892d704fd53a914cd
noppoman <yuki@miketokyo.com>
2016-04-18 17:39:43 +0900

    Add Time module

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ let package = Package(
 	dependencies: [
       .Package(url: "https://github.com/slimane-swift/Middleware.git", majorVersion: 0, minor: 1),
       .Package(url: "https://github.com/slimane-swift/Skelton.git", majorVersion: 0, minor: 3),
+      .Package(url: "https://github.com/slimane-swift/Time.git", majorVersion: 0, minor: 1),
       .Package(url: "https://github.com/slimane-swift/POSIXRegex.git", majorVersion: 0, minor: 4)
   ]
 )
