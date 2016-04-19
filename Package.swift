2cb701415f2629d52dd7ea5cbeec805435073f87
noppoman <yuki@miketokyo.com>
2016-04-20 05:05:45 +0900

    Support AsyncResponderConvertible

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
       .Package(url: "https://github.com/slimane-swift/Middleware.git", majorVersion: 0, minor: 1),
       .Package(url: "https://github.com/slimane-swift/Skelton.git", majorVersion: 0, minor: 3),
       .Package(url: "https://github.com/slimane-swift/Time.git", majorVersion: 0, minor: 1),
-      .Package(url: "https://github.com/slimane-swift/POSIXRegex.git", majorVersion: 0, minor: 4)
+      .Package(url: "https://github.com/slimane-swift/POSIXRegex.git", majorVersion: 0, minor: 4),
+      .Package(url: "https://github.com/slimane-swift/AsyncResponderConvertible.git", majorVersion: 0, minor: 1),
   ]
 )
