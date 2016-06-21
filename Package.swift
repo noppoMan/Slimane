4beba16be19126f8207c78b33d36006741cdad55
noppoman <yuki@miketokyo.com>
2016-06-21 15:18:41 +0900

    Support HTTP Upgrade

--- Package.swift
+++ Package.swift
@@ -3,10 +3,11 @@ import PackageDescription
 let package = Package(
 	name: "Slimane",
 	dependencies: [
-      .Package(url: "https://github.com/slimane-swift/Skelton.git", majorVersion: 0, minor: 7),
+      .Package(url: "https://github.com/noppoMan/Skelton.git", majorVersion: 0, minor: 7),
       .Package(url: "https://github.com/slimane-swift/Time.git", majorVersion: 0, minor: 2),
       .Package(url: "https://github.com/Zewo/POSIXRegex.git", majorVersion: 0, minor: 7),
       .Package(url: "https://github.com/slimane-swift/AsyncResponderConvertible.git", majorVersion: 0, minor: 4),
       .Package(url: "https://github.com/noppoMan/AsyncHTTPSerializer.git", majorVersion: 0, minor: 1),
+      .Package(url: "https://github.com/slimane-swift/HTTPUpgradeAsync.git", majorVersion: 0, minor: 1),
   ]
 )
